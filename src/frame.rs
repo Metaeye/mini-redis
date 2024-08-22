4e197f363a3cbd5fcfdd11a89751b8f46d1a11ad
metaeye <li_yuangang@icloud.com>
2024-08-22 21:02:43 +0800

    chore: Initialize project structure and add basic code templates

---
+++ src/frame.rs
@@ -0,0 +1,301 @@
+//! 提供一个表示 Redis 协议帧的类型以及从字节数组解析帧的实用程序。
+
+use bytes::{Buf, Bytes};
+use std::convert::TryInto;
+use std::fmt;
+use std::io::Cursor;
+use std::num::TryFromIntError;
+use std::string::FromUtf8Error;
+
+/// Redis 协议中的帧。
+#[derive(Clone, Debug)]
+pub enum Frame {
+    Simple(String),
+    Error(String),
+    Integer(u64),
+    Bulk(Bytes),
+    Null,
+    Array(Vec<Frame>),
+}
+
+#[derive(Debug)]
+pub enum Error {
+    /// 没有足够的数据来解析消息
+    Incomplete,
+
+    /// 无效的消息编码
+    Other(crate::Error),
+}
+
+impl Frame {
+    /// 返回一个空数组
+    pub(crate) fn array() -> Frame {
+        Frame::Array(vec![])
+    }
+
+    /// 将一个“bulk”帧推入数组。`self` 必须是一个 Array 帧。
+    ///
+    /// # Panics
+    ///
+    /// 如果 `self` 不是数组，则会 panic
+    pub(crate) fn push_bulk(&mut self, bytes: Bytes) {
+        match self {
+            Frame::Array(vec) => {
+                vec.push(Frame::Bulk(bytes));
+            }
+            _ => panic!("not an array frame"),
+        }
+    }
+
+    /// 将一个“integer”帧推入数组。`self` 必须是一个 Array 帧。
+    ///
+    /// # Panics
+    ///
+    /// 如果 `self` 不是数组，则会 panic
+    pub(crate) fn push_int(&mut self, value: u64) {
+        match self {
+            Frame::Array(vec) => {
+                vec.push(Frame::Integer(value));
+            }
+            _ => panic!("not an array frame"),
+        }
+    }
+
+    /// 检查是否可以从 `src` 解码整个消息
+    pub fn check(src: &mut Cursor<&[u8]>) -> Result<(), Error> {
+        match get_u8(src)? {
+            b'+' => {
+                get_line(src)?;
+                Ok(())
+            }
+            b'-' => {
+                get_line(src)?;
+                Ok(())
+            }
+            b':' => {
+                let _ = get_decimal(src)?;
+                Ok(())
+            }
+            b'$' => {
+                if b'-' == peek_u8(src)? {
+                    // 跳过 '-1\r\n'
+                    skip(src, 4)
+                } else {
+                    // 读取 bulk 字符串
+                    let len: usize = get_decimal(src)?.try_into()?;
+
+                    // 跳过该数量的字节 + 2 (\r\n)。
+                    skip(src, len + 2)
+                }
+            }
+            b'*' => {
+                let len = get_decimal(src)?;
+
+                for _ in 0..len {
+                    Frame::check(src)?;
+                }
+
+                Ok(())
+            }
+            actual => Err(format!("protocol error; invalid frame type byte `{}`", actual).into()),
+        }
+    }
+
+    /// 消息已经通过 `check` 验证。
+    pub fn parse(src: &mut Cursor<&[u8]>) -> Result<Frame, Error> {
+        match get_u8(src)? {
+            b'+' => {
+                // 读取行并将其转换为 `Vec<u8>`
+                let line = get_line(src)?.to_vec();
+
+                // 将行转换为 String
+                let string = String::from_utf8(line)?;
+
+                Ok(Frame::Simple(string))
+            }
+            b'-' => {
+                // 读取行并将其转换为 `Vec<u8>`
+                let line = get_line(src)?.to_vec();
+
+                // 将行转换为 String
+                let string = String::from_utf8(line)?;
+
+                Ok(Frame::Error(string))
+            }
+            b':' => {
+                let len = get_decimal(src)?;
+                Ok(Frame::Integer(len))
+            }
+            b'$' => {
+                if b'-' == peek_u8(src)? {
+                    let line = get_line(src)?;
+
+                    if line != b"-1" {
+                        return Err("protocol error; invalid frame format".into());
+                    }
+
+                    Ok(Frame::Null)
+                } else {
+                    // 读取 bulk 字符串
+                    let len = get_decimal(src)?.try_into()?;
+                    let n = len + 2;
+
+                    if src.remaining() < n {
+                        return Err(Error::Incomplete);
+                    }
+
+                    let bytes = Bytes::copy_from_slice(&src.chunk()[..len]);
+
+                    // 跳过该数量的字节 + 2 (\r\n)。
+                    skip(src, n)?;
+
+                    Ok(Frame::Bulk(bytes))
+                }
+            }
+            b'*' => {
+                let len = get_decimal(src)?.try_into()?;
+                let mut vec = Vec::with_capacity(len);
+
+                for _ in 0..len {
+                    vec.push(Frame::parse(src)?);
+                }
+
+                Ok(Frame::Array(vec))
+            }
+            _ => unimplemented!(),
+        }
+    }
+
+    /// 将帧转换为“unexpected frame”错误
+    pub(crate) fn to_error(&self) -> crate::Error {
+        format!("unexpected frame: {}", self).into()
+    }
+}
+
+impl PartialEq<&str> for Frame {
+    fn eq(&self, other: &&str) -> bool {
+        match self {
+            Frame::Simple(s) => s.eq(other),
+            Frame::Bulk(s) => s.eq(other),
+            _ => false,
+        }
+    }
+}
+
+impl fmt::Display for Frame {
+    fn fmt(&self, fmt: &mut fmt::Formatter) -> fmt::Result {
+        use std::str;
+
+        match self {
+            Frame::Simple(response) => response.fmt(fmt),
+            Frame::Error(msg) => write!(fmt, "error: {}", msg),
+            Frame::Integer(num) => num.fmt(fmt),
+            Frame::Bulk(msg) => match str::from_utf8(msg) {
+                Ok(string) => string.fmt(fmt),
+                Err(_) => write!(fmt, "{:?}", msg),
+            },
+            Frame::Null => "(nil)".fmt(fmt),
+            Frame::Array(parts) => {
+                for (i, part) in parts.iter().enumerate() {
+                    if i > 0 {
+                        // 使用空格作为数组元素显示分隔符
+                        write!(fmt, " ")?;
+                    }
+
+                    part.fmt(fmt)?;
+                }
+
+                Ok(())
+            }
+        }
+    }
+}
+
+fn peek_u8(src: &mut Cursor<&[u8]>) -> Result<u8, Error> {
+    if !src.has_remaining() {
+        return Err(Error::Incomplete);
+    }
+
+    Ok(src.chunk()[0])
+}
+
+fn get_u8(src: &mut Cursor<&[u8]>) -> Result<u8, Error> {
+    if !src.has_remaining() {
+        return Err(Error::Incomplete);
+    }
+
+    Ok(src.get_u8())
+}
+
+fn skip(src: &mut Cursor<&[u8]>, n: usize) -> Result<(), Error> {
+    if src.remaining() < n {
+        return Err(Error::Incomplete);
+    }
+
+    src.advance(n);
+    Ok(())
+}
+
+/// 读取一个以新行终止的十进制数
+fn get_decimal(src: &mut Cursor<&[u8]>) -> Result<u64, Error> {
+    use atoi::atoi;
+
+    let line = get_line(src)?;
+
+    atoi::<u64>(line).ok_or_else(|| "protocol error; invalid frame format".into())
+}
+
+/// 查找一行
+fn get_line<'a>(src: &mut Cursor<&'a [u8]>) -> Result<&'a [u8], Error> {
+    // 直接扫描字节
+    let start = src.position() as usize;
+    // 扫描到倒数第二个字节
+    let end = src.get_ref().len() - 1;
+
+    for i in start..end {
+        if src.get_ref()[i] == b'\r' && src.get_ref()[i + 1] == b'\n' {
+            // 找到一行，更新位置到 \n 之后
+            src.set_position((i + 2) as u64);
+
+            // 返回该行
+            return Ok(&src.get_ref()[start..i]);
+        }
+    }
+
+    Err(Error::Incomplete)
+}
+
+impl From<String> for Error {
+    fn from(src: String) -> Error {
+        Error::Other(src.into())
+    }
+}
+
+impl From<&str> for Error {
+    fn from(src: &str) -> Error {
+        src.to_string().into()
+    }
+}
+
+impl From<FromUtf8Error> for Error {
+    fn from(_src: FromUtf8Error) -> Error {
+        "protocol error; invalid frame format".into()
+    }
+}
+
+impl From<TryFromIntError> for Error {
+    fn from(_src: TryFromIntError) -> Error {
+        "protocol error; invalid frame format".into()
+    }
+}
+
+impl std::error::Error for Error {}
+
+impl fmt::Display for Error {
+    fn fmt(&self, fmt: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            Error::Incomplete => "stream ended early".fmt(fmt),
+            Error::Other(err) => err.fmt(fmt),
+        }
+    }
+}
